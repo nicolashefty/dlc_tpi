8c15c7edee7baf7b936ac95c5e64a74be0f654cf
Nicolas Hefty <nicoehefty@gmail.com>
2016-03-24 13:27:59 -0300

    Watching for empty search

--- scripts/search.js
+++ scripts/search.js
@@ -2,5 +2,9 @@ document.getElementById("searchField").onsearch = function() {search()};
 document.getElementById("searchbtn").onclick = function() {search()};
 function search() {
 	var x = document.getElementById("searchField");
-	document.getElementById("searchkey").innerHTML = "You are searching for: " + x.value;
+	if(x.value != "")
+	{
+		document.getElementById("searchkey").innerHTML = "You are searching for: " + x.value;
+	}
+	//window.alert(5 + 6);
 }
