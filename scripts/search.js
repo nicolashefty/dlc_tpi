8faac09f6de9998fe5d62c4e9184e8ec9a17a9f0
Nicolas Hefty <nicoehefty@gmail.com>
2016-03-24 13:01:42 -0300

    Adding the function to the search button

--- scripts/search.js
+++ scripts/search.js
@@ -1,5 +1,5 @@
 document.getElementById("searchField").onsearch = function() {search()};
-
+document.getElementById("searchbtn").onclick = function() {search()};
 function search() {
 	var x = document.getElementById("searchField");
 	document.getElementById("searchkey").innerHTML = "You are searching for: " + x.value;
